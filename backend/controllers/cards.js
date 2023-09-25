cdb238a5ab18cd1c0e27d986c187a68c0510b83f
Svetlana Lutkova <lilissiya@yandex.ru>
2023-09-25 06:20:18 +0500

    Тест

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -18,7 +18,7 @@ module.exports.createCard = (req, res, next) => {
 };
 module.exports.getCards = (req, res, next) => {
   Card.find({})
-    .then((cards) => res.send({ data: cards }))
+    .then((cards) => res.send({ cards }))
     .catch((err) => next(err));
 };
 module.exports.deleteCard = (req, res, next) => {
